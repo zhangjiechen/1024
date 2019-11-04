30da8dfdd8763555cacc4fab05fdbe5a6dd48ca9
zhangjiechen <1163349258@qq.com>
2019-11-04 14:08:49 +0800

    提交代码

---
+++ 11_4/11_4/test.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+//int main()
+//{
+//	int a=10;
+//	int *p=&a;
+//	printf("%d\n",sizeof(p));
+//	*p=20;
+//	printf("%p\n",&a);
+//	printf("%d",*p);
+//	return 0;
+//
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%d\n",sizeof(char*));
+//	printf("%d\n",sizeof(short*));
+//	printf("%d\n",sizeof(int*));
+//	printf("%d\n",sizeof(long*));
+//	printf("%d\n",sizeof(float*));
+//
+//
+//	return 0;
+//}
+//struct Stu
+//{
+//	char name[20];
+//	short age;
+//	char sex[5];
+//	char id[10];
+//};
+//int main()
+//{
+//	struct Stu zhangsan={"ÕÅÈý",15,"ÄÐ","10111602"};
+//	struct Stu *pzs=NULL;
+//	pzs=&zhangsan;
+//	printf("%s %d %s %s\n",zhangsan.name,zhangsan.age,zhangsan.sex,zhangsan.id);
+//	printf("%s %d %s %s\n",*pzs->name,(*pzs).age,zhangsan.sex,zhangsan.id);
+//	return 0;
+//
+//}
+int main()
+{
+	int a=5;
+	if(1==a)
+	{
+		printf("bingo!\n");
+	}
+	else
+	{
+		printf("budui\n");
+	}
+	return 0;
+
+}
