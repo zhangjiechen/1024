714a3b3f1cd94b0fa34abd94e121162b4a1c7707
zhangjiechen <1163349258@qq.com>
2019-11-04 20:11:04 +0800

    更新代码

--- 11_4/11_4/test.c
+++ 11_4/11_4/test.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+//#include <stdio.h>
 //int main()
 //{
 //	int a=10;
@@ -39,17 +39,85 @@
 //	return 0;
 //
 //}
+//int main()
+//{
+//	int a = 5;
+//	if(1 == a)
+//	{
+//		printf("bingo!\n");
+//	}
+//	else
+//	{
+//		printf("budui\n");
+//	}
+//	return 0;
+//
+//}
+//int main()
+//{
+//	**int ch = getchar();
+//	putchar(ch);
+//	printf("\n");
+//	ch = getchar();
+//	putchar(ch);*/*/
+//	/*int ch = 0;*/
+//	/*while((ch = getchar())!=EOF)
+//	{
+//		if('0'<ch&&ch<'9')
+//		printf("%c\n",ch);
+//	}*/
+//	/*while((ch=getchar())!=EOF)
+//	{
+//		if(ch<'0'||ch>'9')
+//			continue;
+//		putchar(ch);
+//	}*/
+#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	for(i=1; i<=10; i++)
+//	{
+//		if(i==5)
+//			continue;
+//		printf("%d ",i);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	int k = 0;
+//	for(i=0,k=0;k=0;i++,k++)
+//		k++;
+//	return 0;
+//}
 int main()
 {
-	int a=5;
-	if(1==a)
+	int arr[] = {1,2,3,4,5,6,7,8,9,10};
+	int left = 0;
+	int right = 9;
+	int i = 7;
+	while(left<=right)
 	{
-		printf("bingo!\n");
+		int mid = (left+right)/2;
+		if(arr[mid]>i)
+		{
+			right=mid-1;
+		}
+		else if(arr[mid]<i)
+		{
+			left = mid+1;
+		}
+		else
+		{
+			printf("下标是:%d\n",mid);
+			break;
+		}
 	}
-	else
+	if(left>right)
 	{
-		printf("budui\n");
+		printf("找不到!");
 	}
 	return 0;
-
 }
