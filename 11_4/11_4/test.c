33e61502e341e9f3c3ed26bb202e7fd085951beb
zhangjiechen <1163349258@qq.com>
2019-11-06 18:22:37 +0800

    更新代码

--- 11_4/11_4/test.c
+++ 11_4/11_4/test.c
@@ -244,20 +244,20 @@
 //	}
 //	return 0;
 ////}
-//#include <windows.h>
-//int main()
-//{
-//	char input[20]={0};
-//	system("shutdown -s -t 60");
-//	while(1)
-//		{
-//			printf("请注意，你的电脑将在一分钟内关机，如果输入:我是猪 就取消关机\n");
-//			scanf("%s",&input);
-//			if(strcmp(input,"我是猪")==0)
-//			{
-//			system("shutdown -a");
-//			break;
-//			}
-//	}
-//	return 0;
-//}
+#include <windows.h>
+int main()
+{
+	char input[20]={0};
+	system("shutdown -s -t 60");
+	while(1)
+		{
+			printf("请注意，你的电脑将在一分钟内关机，如果输入:我是猪 就取消关机\n");
+			scanf("%s",&input);
+			if(strcmp(input,"我是猪")==0)
+			{
+			system("shutdown -a");
+			break;
+			}
+	}
+	return 0;
+}
